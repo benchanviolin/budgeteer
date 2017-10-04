8e68092fe1c1c41073b91b6e4ec796e5629e41cb
Ben Chan <benchanviolin@gmail.com>
2017-10-04 07:43:53 -0400

    type: feat | Prepare for responsive header.

--- client/src/components/headers/material-ui/MyAppBar.js
+++ client/src/components/headers/material-ui/MyAppBar.js
@@ -11,23 +11,58 @@ import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 //import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
+const menuItems = [
+  {
+    name: 'Login',
+    conditions: [
+      'logged-in': false
+    ],
+  },
+  {
+    name: 'Logout',
+    conditions: [
+      'logged-in': true
+    ]
+  }
+];
+
 export default function MyAppBar(props) {
   return (
     <AppBar
-      title={props.title}
+      title={
+        <a className="App-a" href="">
+          <img className="App-logo" src={logo} alt="logo" />
+          <span className="App-title">{props.title}</span>
+        </a>
+      }
       onLeftIconButtonTouchTap={props.onPressLeftMenu}
-      iconElementLeft={<IconButton><img className="App-logo" src={logo} alt="logo" /></IconButton>}
+      iconElementLeft={
+        <IconButton>
+        </IconButton>
+      }
       iconElementRight={
-        <IconMenu
-          iconButtonElement={
-            <IconButton><MoreVertIcon /></IconButton>
-          }
-          targetOrigin={{horizontal: 'right', vertical: 'top'}}
-          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-        >
-          <MenuItem primaryText="Login" />
-          <MenuItem primaryText="Sign out" />
-        </IconMenu>
+        <div>
+          <span class="App-menu-items">
+          {menuItems.map((item, key) => 1 == 1
+            ? <span class="App-menu-item" key={key}>{item.name}</span>
+            : ''
+          )}
+          </span>
+          <IconMenu
+            iconButtonElement={
+              <IconButton>
+                <MoreVertIcon className="App-vertical-icon" />
+              </IconButton>
+            }
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          >
+            {menuItems.map((item, key) => 1 == 1
+              ? <MenuItem key={key} primaryText={item.name} />
+              : ''
+            )}
+          </IconMenu>
+        </div>
       }
     />
   )
