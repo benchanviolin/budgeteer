1d18ae7ebd3cc983bd5ff00be9058db622f375bd
Ben Chan <benchanviolin@gmail.com>
2017-10-06 19:53:14 -0400

    type: feat | Add edit to bear page.

--- admin/src/App.js
+++ admin/src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './views/dashboard';
 import AuthClient from './clients/authClient';
 import RestClient from './clients/restClient';
 import { UserList } from './views/users';
-import { BearList } from './views/bears';
+import { BearList, BearEdit } from './views/bears';
 import PostIcon from 'material-ui/svg-icons/action/book';
 import UserIcon from 'material-ui/svg-icons/social/group';
 
@@ -13,7 +13,7 @@ const App = () => (
     <Admin dashboard={Dashboard} authClient={AuthClient} restClient={RestClient}>
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} remove={Delete} icon={PostIcon} />
         <Resource name="users" list={UserList} icon={UserIcon} />
-        <Resource name="bears" list={BearList} />
+        <Resource name="bears" list={BearList} edit={BearEdit} />
     </Admin>
 );
 
--- admin/src/views/bears.js
+++ admin/src/views/bears.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'admin-on-rest';
+import { Edit, List, Responsive, SimpleList, Datagrid, Filter, TextField, EditButton, TextInput, SimpleForm, DisabledInput } from 'admin-on-rest';
 
 export const BearList = (props) => (
-    <List title="All bears" {...props}>
-        <Datagrid>
-            <TextField source="id" />
-            <TextField source="name" />
-        </Datagrid>
-    </List>
+  <List {...props} filters={<BearFilter />}>
+    <Responsive
+      small={
+          <SimpleList
+              primaryText={record => record.name}
+              secondaryText={record => record.id}
+              tertiaryText={record => `yo`}
+          />
+      }
+      medium={
+          <Datagrid>
+              <TextField source="id" />
+              <TextField source="name" />
+              <EditButton />
+          </Datagrid>
+      }
+    />
+  </List>
+);
+
+const BearFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+    </Filter>
+);
+
+const BearTitle = ({ record }) => {
+    return <span>Bear {record ? `"${record.name}"` : ''}</span>;
+};
+
+export const BearEdit = (props) => (
+    <Edit title={<BearTitle />} {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <TextInput source="name" />
+        </SimpleForm>
+    </Edit>
 );
