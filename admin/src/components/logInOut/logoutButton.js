46748b779318ffb0a11bbd76dc3371870e1a6833
Ben Chan <benchanviolin@gmail.com>
2017-10-09 20:39:46 -0400

    type: feat | Add Facebook auth.

---
+++ admin/src/components/logInOut/logoutButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { userLogout } from 'admin-on-rest';
+
+const LogoutButton = ({ userLogout }) => (
+    <button onClick={userLogout}>Logout</button>
+);
+
+export default connect(undefined, { userLogout })(LogoutButton);
